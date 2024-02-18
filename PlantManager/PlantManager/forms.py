20e1bd78ddeca88988676a6ef6aaac721072341c
ArtilleroArgento <emmanuelramosoficial@gmail.com>
2024-02-18 18:42:38 -0300

    Este es mi primer commit

--- PlantManager/PlantManager/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Plant
-
-class PlantForm(forms.ModelForm):
-    class Meta:
-        model = Plant
-        fields = ['name', 'origin_zone', 'care_instructions']
--- PlantManager/PlantManager/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Plant(models.Model):
-    name = models.CharField(max_length=100)
-    origin_zone = models.CharField(max_length=100)
-    care_instructions = models.TextField()
-
-   
-
